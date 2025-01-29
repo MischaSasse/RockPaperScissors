96755f7b2aadff5db2e4ea14e0f29efa1d47a81c
Mischa Sasse <mmischaaaa4@gmail.com>
2025-01-29 19:31:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Rock Paper Scissors\n" +
+            "Please pick one of the three options:\n" +
+            "1: Rock, 2: Paper, 3: Scissors");
+    a:
+        int userInput = GetUserInput();
+        if (userInput == 0) { Error(); goto a; };
+        int computerInput = ComputerInput();
+        Console.WriteLine(Result(userInput, computerInput));
+    }
+    private static int GetUserInput()
+    {
+        string userInput = Console.ReadLine().Trim().ToLower();
+        return MapInput(userInput);
+    }
+
+    private static int MapInput(string? input)
+    {
+        return input switch
+        {
+            "rock" or "1" => 1,
+            "paper" or "2" => 2,
+            "scissors" or "3" => 3,
+            _ => 0
+        };
+    }
+    private static int ComputerInput()
+    {
+        Random randomNumber = new();
+        return randomNumber.Next(1, 4);
+    }
+
+    private static string Result(int user, int computer)
+    {
+        if (computer - user == 1 || computer - user == -2) { return "Computer has won with " + ResultMap(computer); }
+        if (computer - user == -1 || computer - user == 2) { return "You won, computer chose " + ResultMap(computer); }
+        return "Tie, the computer also chose " + ResultMap(computer);
+    }
+
+    private static void Error()
+    {
+        Console.WriteLine("no valid input, please try again");
+    }
+
+    private static string ResultMap(int computerInput)
+    {
+        return computerInput switch
+        {
+            1 => "rock",
+            2 => "paper",
+            3 => "scissors",
+            _ => "error",
+        };
+    }
+}
